bb9a0d9d8de19363bd0e23951aff739bee2f9e79
lizifeng1209 <1779397038@qq.com>
2024-08-14 23:27:54 +0800

    Delete DivBSnew.sh

--- DivBSnew.sh
+++
@@ -1,5 +0,0 @@
-CUDA_VISIBLE_DEVICES=1,2 python main.py --cfg cfg/cifar100_DivBSnew_01.yaml --seed 0;
-
-CUDA_VISIBLE_DEVICES=1,2 python main.py --cfg cfg/cifar10_DivBSnew_01.yaml --seed 0;
-
-CUDA_VISIBLE_DEVICES=1,2 python main.py --cfg cfg/tinyimagenet_DivBSnew_01.yaml --seed 0;
